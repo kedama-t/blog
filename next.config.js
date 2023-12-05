1dd5dbb1c24c8e358177767db71c772b32b9d279
kedama-t <mt.masaaki.takeda+github@gmail.com>
2023-12-05 17:49:02 +0900

    依存関係の修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { swcMinify: false };
 
 module.exports = withContentlayer(nextConfig);
